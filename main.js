9e11ae8471220a0b7b8105d08003fa7a46e824f5
ronicleal <ralealp01@iesalbarregas.es>
2025-11-26 23:02:06 +0100

    ✅new Escena1 del main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*====IMPORTACION DE CLASES====*/
 import { Jugador } from "./modules/jugadores.js";
+import { showScene } from "./utils/utils.js";
 
 
 /*====VARIABLES GLOBALES====*/
@@ -13,11 +14,53 @@ function escena1() {
 
     btnCrear.addEventListener("click", function() {
         const nombre = document.getElementById("nombre-jugador").value.trim();
-
         console.log(nombre);
+
+        if(!nombre){
+            alert("¡Debes introducir un nombre!")
+        }
+
+        //Crear jugador
+        jugador = new Jugador(nombre);
+
+        //Mostrar el nombre en el DOM
+        document.getElementById("nombre-jugador-display").textContent = jugador.nombre;
+
+        //Mostrar estado inicial del jugador en la misma escena
+        const estadoDiv = document.getElementById("estado-jugador");
+
+        estadoDiv.innerHTML = `
+            <div class="stats-grid">
+                <div class="stat-box">⚔️ Ataque: ${jugador.ataqueTotal}</div>
+                <div class="stat-box">🛡️ Defensa: ${jugador.defensaTotal}</div>
+                <div class="stat-box">❤️ Vida: ${jugador.vida} / ${jugador.vidaMax}</div>
+                <div class="stat-box">⭐ Puntos: ${jugador.puntos}</div>
+            </div>
+        `;
+
+        let btnContinuar = document.getElementById("continuar-mercado");
+        if(!btnContinuar) {
+            btnContinuar = document.createElement("button");
+            btnContinuar.id = "continuar-mercado";
+            btnContinuar.textContent = "➡️Continuar Mercado";
+            estadoDiv.appendChild(btnContinuar);
+
+            btnContinuar.addEventListener("click", () => {
+                showScene("market");
+                escena2();
+
+            });
+        }
+
 
 
     });
+}
+
+
+/*====ESCENA 2: MOSTRAR EL MERCADO DE PRODUCTOS ====*/
+function escena2(){
+
 }
 
 //Inicio del juego
