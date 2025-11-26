d5267fc29f53718475ce48942e1fc1e0a74337be
ronicleal <ralealp01@iesalbarregas.es>
2025-11-26 11:25:21 +0100

    ✅Creacion Clase Jugador.js

---
+++ modules/jugadores.js
@@ -0,0 +1,112 @@
+export class Jugador {
+    nombre;
+    puntos;
+    inventario;
+    vidaMax;
+    vida;
+
+    /**
+    * Crea una nueva instancia de Jugador.
+    * @param {string} nombre - Nombre del jugador.
+    */
+    constructor(nombre, avatar = './imagenes/player.png') {
+        this.nombre = nombre;
+        this.avatar = avatar;
+        this.puntos = 0;
+        this.inventario = [];
+        this.vidaMax = 100;
+        this.vida = this.vidaMax;
+    }
+
+    /**
+    * Añade un objeto al inventario del jugador.
+    * Se utiliza `structuredClone` para evitar modificar el objeto original.
+    * @param {Object} item - Objeto que se añadirá al inventario.
+    */
+    añadirItem(item) {
+        this.inventario.push(structuredClone(item));
+    }
+
+
+    /**
+     * Incrementa los puntos del jugador.
+    * @param {number} cantidad -Cantidad de puntos a añadir.
+    */
+    ganarPuntos(cantidad) {
+        this.puntos += cantidad;
+    }
+
+    /**
+    * Calcula el total de ataque del jugador basado en los bonus de sus ítems de tipo 'Arma'.
+    * @returns {number} Puntos de ataque totales.
+    */
+    get ataqueTotal() {
+        return this.inventario.reduce((total, item) =>
+            total + (item.tipo === 'Arma' ? item.bonus : 0), 0);
+
+    }
+
+
+    /**
+    * Getter de Defensa Total (basado en items de tipo 'Armadura')
+    * @returns {number} Puntos de defensa totales.
+    */
+    get defensaTotal() {
+        return this.inventario.reduce((total, item) =>
+            total + (item.tipo === 'Armadura' ? item.bonus : 0), 0);
+    }
+
+    /**
+    * Getter de Vida Total (basado en items de tipo 'Consumible' que dan vida)
+    * @returns {number} Puntos de vida máxima totales.
+    */
+    get vidaTotal() {
+        const bonusVida = this.inventario.reduce((total, item) =>
+            total + (item.tipo === 'Consumible' ? item.bonus : 0), 0);
+
+        return this.vidaMax + bonusVida;
+    }
+
+    /**
+     * Agrupa los ítems del inventario por tipo.
+     * @returns {Object} Un objeto con listas de objetos agrupados por tipo.
+     */
+    inventarioPorTipo() {
+        return groupBy(this.inventario, item => item.tipo);
+    }
+
+
+
+    /**
+    * Devuelve una presentación detallada del jugador.
+    * @returns {Object} Descripción formateada del jugador.
+    */
+    mostrarJugador() {
+        return `
+      👤 ${this.nombre}
+      ❤️ Vida: ${this.vida}/${this.vidaMax}
+      ⭐ Puntos: ${this.puntos}
+      ⚔️ Ataque total: ${this.ataqueTotal}
+      🛡️ Defensa total: ${this.defensaTotal}
+      🎒 Inventario: ${this.inventario.length > 0
+                ? this.inventario.map(item => item.nombre).join(', ')
+                : 'Vacío'}
+    `;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
